738bb584a5e29de5ebeb3134338f82bbf9e66718
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2020-11-14 08:57:44 -0800

    Intial commit

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -76,10 +76,10 @@ TEMPLATES = [
     },
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
 # EMAIL_HOST_USER = 'apunguray@gmail.com'
 # EMAIL_HOST_PASSWORD = '0546227605'
 
